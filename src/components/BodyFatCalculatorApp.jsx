2c4b17495b69e2bae9d972ada77d4e1fe1f3df88
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2025-03-03 14:35:22 +0000

    u

--- src/components/BodyFatCalculatorApp.jsx
+++ src/components/BodyFatCalculatorApp.jsx
@@ -74,13 +74,6 @@ const BodyFatCalculatorApp = ({ onBack }) => {
         }
       };
 
-      const calculateDaysBetween = (date1, date2) => {
-        const d1 = new Date(date1);
-        const d2 = new Date(date2);
-        const diffTime = Math.abs(d2 - d1);
-        return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-      };
-    
     return (
         <div className="body-fat-calculator">
             <h1>Body Fat Calliper Calculator</h1>

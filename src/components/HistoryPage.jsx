c5961d5b9773ed962da7f8a6a26ff792753271c6
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2025-03-05 13:49:34 +0000

    chart(graph)

--- src/components/HistoryPage.jsx
+++ src/components/HistoryPage.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 
 const HistoryPage = ({ onBack }) => {
@@ -27,9 +32,48 @@ const HistoryPage = ({ onBack }) => {
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   };
 
+
+        // Prepare data for the graph
+      const chartData = {
+  labels: history.map((entry) => entry.date), // Labels based on the dates
+  datasets: [
+    {
+      label: 'Weight (kg)',
+      data: history.map((entry) => entry.measurements.weight),
+      borderColor: 'rgb(75, 192, 192)',
+      backgroundColor: 'rgba(75, 192, 192, 0.2)',
+      fill: false,
+    },
+    {
+      label: 'Fat Mass (%)',
+      data: history.map((entry) => entry.measurements.fat),
+      borderColor: 'rgb(255, 99, 132)',
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      fill: false,
+    },
+    {
+      label: 'Muscle Mass (%)',
+      data: history.map((entry) => entry.measurements.muscle),
+      borderColor: 'rgb(54, 162, 235)',
+      backgroundColor: 'rgba(54, 162, 235, 0.2)',
+      fill: false,
+    },
+    {
+      label: 'Water (%)',
+      data: history.map((entry) => entry.measurements.water),
+      borderColor: 'rgb(153, 102, 255)',
+      backgroundColor: 'rgba(153, 102, 255, 0.2)',
+      fill: false,
+    }
+  ]
+};
+
+
   return (
     <div>
       <h1 id="Historyh2">Measurement History</h1>
+   {/* Render Chart.js Line Chart */}
+   {history.length > 0 && ( <div> <Line data={chartData} options={{ responsive: true }} /> </div> )}
       <ul id="historyul"> {history.map((entry, index) => (
           <div key={index}> {index > 0 && (
               <h2> {calculateDaysBetween(history[index - 1].date, entry.date)} days between last measurement </h2> )}

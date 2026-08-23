1960c89bdfc93c1db20b8657417c30cd4ad9e9fb
Kostas9093 <159778628+Kostas9093@users.noreply.github.com>
2026-08-23 13:17:41 +0100

    remove history

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { seedHistory } from './seedData.js'
-
-// Load imported history on a fresh install (does nothing if data already exists).
-seedHistory()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
